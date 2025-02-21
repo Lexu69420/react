afb5648a1b69cdebb45cd3f33fb413f193e155cc
Lexuuu11 <jepoylao@gmail.com>
2025-02-21 16:50:24 +0800

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react";
 import {
-  Route, 
-  createBrowserRouter, 
-  createRoutesFromElements, 
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import MainLayout from './layouts/MainLayout';
-import Todos from './pages/Todos';
-import Todo from './pages/Todo';
+import MainLayout from "@/layouts/MainLayout";
+import Todos from "./pages/Todos";
+import Todo from "./pages/Todo";
+import AddTodo from "./pages/AddTodo.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout/>}>
-      <Route index element={<Todos/>}/>
-      <Route path="/todo/:id" element={<Todo/>}/>
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Todos/>} />
+      <Route path="/todo/:id" element={<Todo/>} />
+      <Route path="/add-todo" element={<AddTodo/>} />
     </Route>
   )
 );
 
 const App = () => {
-  return <RouterProvider router={router}/>;
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
